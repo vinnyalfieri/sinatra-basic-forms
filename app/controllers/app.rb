99361eafcd87f9050ce77155731add98f54ce9f0
makafis <vinnyalfieri@gmail.com>
2015-10-20 15:32:57 -0400

    labreview

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -6,10 +6,37 @@ class App < Sinatra::Base
     erb :'songs/index'
   end
 
+  get '/songs/new' do 
+    erb :'songs/new'
+  end
+
+  post '/songs/new' do
+    @song = Song.create(params[:song])
+    erb :'songs/show'
+  end
+
   get '/songs/:id' do
     @song = Song.find(params[:id])
     erb :'songs/show'
   end
+
+  get '/songs/:id/edit' do 
+    @song = Song.find(params[:id])
+    erb :'songs/edit'
+  end
+
+  post '/songs/:id' do
+    @song = Song.find(params[:id])
+    @song.update(params[:song])
+    erb :'songs/show'
+  end
+
+  delete '/songs/:id' do 
+    @song = Song.find(params[:id])
+    @song.destroy
+    redirect '/'
+  end
   
+
 end
 
